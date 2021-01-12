7f48142b92a48e0f8c4eb2354267c18802b450fe
lumilock <thibaud.perrin@outlook.com>
2021-01-12 20:23:55 +0100

    add Service connection

--- App/Http/Controllers/GatewayController.php
+++ App/Http/Controllers/GatewayController.php
@@ -3,6 +3,7 @@
 namespace lumilock\lumilockGateway\App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 use lumilock\lumilockGateway\App\Services\RouteService;
 use lumilock\lumilockGateway\App\Traits\ApiResponser;
@@ -28,38 +29,34 @@ class GatewayController extends Controller
      */
     public function routesGet(Request $request, $route)
     {
-        dd('herh0');
-        return response()->json(
-            [
-                'data' => null,
-                'status' => 'NOT_FOUND',
-                'message' => 'Service not found!'
-            ],
-            404
-        );
         // // we create a list of slug
-        // $slug_array = preg_split("/\//", $request->path());
+        $slug_array = preg_split("/\//", $request->path());
         // // we check that we have 2 slug (api / <serviceName>)
-        // if (count($slug_array) >= 2 && strcmp($slug_array[0], "api") === 0) {
-        //     try{
-        //         $path = '/' . $slug_array[0] . '/' . $slug_array[1]; // we re-create the service path in order to find the uri
+        if (count($slug_array) >= 2 && strcmp($slug_array[0], "api") === 0) {
+            try {
+                $path = '/' . $slug_array[0] . '/' . $slug_array[1]; // we re-create the service path in order to find the uri
 
-        //         $service = new ServiceUriResource(Service::where('path', '=', $path)->firstOrFail()); // we get uri and secret from database if exist
-        //         $this->routeService->setUri($service->uri);
-        //         $this->routeService->setSecret($service->secret);
-        //         $headers['Authorization'] = $request->header('Authorization');
-        //         return $this->successResponse($this->routeService->route($request->method(), $request->path(), [], $headers));
-        //     } catch (\Exception $e) {
-        //         return response()->json(
-        //             [
-        //                 'data' => null,
-        //                 'status' => 'NOT_FOUND',
-        //                 'message' => 'Service not found!'
-        //             ],
-        //             404
-        //         );
-        //     }
-        // }
+                // we get the service from the Auth micro-service lumen app
+                $service_response = json_decode($this->routeService->route('POST', env("AUTH_URI") . '/api/auth/services', ["path" => $path]));
+                if (!$service_response) {
+                    throw new Exception("Error Processing Request", 1);
+                }
+                $service = $service_response->data;
+                $this->routeService->setUri($service->uri);
+                $this->routeService->setSecret($service->secret);
+                $headers['Authorization'] = $request->header('Authorization');
+                return $this->successResponse($this->routeService->route($request->method(), $request->path(), [], $headers));
+            } catch (\Exception $e) {
+                return response()->json(
+                    [
+                        'data' => null,
+                        'status' => 'NOT_FOUND',
+                        'message' => 'Service not found!'
+                    ],
+                    404
+                );
+            }
+        }
     }
 
     /**
